55a12c8f9e834792ca17eaedc0090b353fc131ae
zhangli344236745@163.com <zhangli344236745@163.com>
2016-12-01 14:17:09 +0800

    New changelist

--- twi/t5.py
+++ twi/t5.py
@@ -1,5 +1,11 @@
-__author__ = '0138695'
+# -*- coding:utf-8 -*-
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 brower = webdriver.Firefox()
-brower.get("http://www.baidu.com")
+brower.get("http://www.tianyancha.com")
+search_input = brower.find_element_by_id("live-search")
+search_input.send_keys(u"星巴克")
+search_input.send_keys(Keys.ENTER)
+
